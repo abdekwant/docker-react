e879bed3bb18a8fa0557f0d2f08d59e7862d96d7
Ab de Kwant <ab@dekwant.com>
2022-01-19 07:51:30 -0500

    Changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Corrected the Error Here
+          Learn React MY CHANGE
         </a>
       </header>
     </div>
